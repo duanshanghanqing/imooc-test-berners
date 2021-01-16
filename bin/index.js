9138486166a729bb5a46f288c5349fd15a4323cb
gaojunfeng <490771426@qq.com>
2021-01-16 15:37:18 +0800

    学习commander

--- bin/index.js
+++ bin/index.js
@@ -1,58 +1,113 @@
 #!/usr/bin/env node
-// 使用当前操作系统node环境执行当前脚本文件
 
-/*
-console.log('welcome imooc-test-berners');
+// commander 会自动实现 -V -h 功能
+// ode index.js  --version
 
-// 注册一个命令 imooc-test-berners init
+// const { program } = require('commander');
 
-// 实现参数解析 --version 和 init --name
+const { Command } = require('commander');
+const pkg = require('../package.json');
 
-const { argv } = require('process');
-console.log(argv);
-*/
+const program = new Command();
 
-
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const dedent = require('dedent');
-const cli = yargs(hideBin(process.argv));
-
-cli
-    .usage('Usage: imooc-test [command] <options>') // 提醒
-    .demandCommand(1, 'A command is required. Pass --help to see all available commands and options.') // 最少输入 Command 的数量，表示最少输入一个命令
-    .strict()
-    .alias('h', 'help') // 别名, 给help 起了个别名
-    .alias('v', 'version') // 别名, 给version 起了个别名
-    // .wrap(100) // 命令行宽度
-    .wrap(cli.terminalWidth()) // 获取命令行宽度，定格显示
-    .epilogue(
-        // 让其顶行显示
-        dedent`
-            When a command fails, all logs are written to lerna-debug.log in the current working directory.
-
-            For more information, find our manual at https://github.com/lerna/lerna
-        `
-    ) // 页脚添加一段话
-    .option({ // 添加一个选项命令，可设置多个
-        debug: {// 添加 debug 选项命令
-            type: 'boolean',
-            describe: 'Bootstrap debug mode',
-            alias: 'd' // 别名，也可以通过 .alias('d', 'debug')  方法设置，通过设置位置排序实现排序
-        }
-    })
-    .option('registry', {
-        type: 'string',
-        // hidden: true, // 在控制台隐藏此命令，但可以使用
-        describe: 'Define global registry',
-    })
-    .argv;
-
-if (argv.ships > 3 && argv.distance < 53.5) {
-    console.log('Plunder more riffiwobbles!');
-} else {
-    console.log('Retreat from the xupptumblers!');
-}
+// 基本使用,注册参数
+program
+    .name(Object.keys(pkg.bin)[0])// Usage: imooc-test-berners <command> [options] 实现nanme
+    .usage('<command> [options]') // Usage: imooc-test-berners <command> [options] 实现后面两个参数
+    .version(pkg.version)
+    .option('-d, --debug', '是否开启调试模式', false) // 默认值为 false
+    .option('-e, --envName <envName>', '获取环境变量名称')
 
 
+// 相关方法
+// program.outputHelp();
 
+// 打印参数
+// const options = program.opts();
+// console.log(options.debug);
+// console.log(options.envName);
+
+
+// 注册command命令为主命令，方式1, 参考 https://github.com/tj/commander.js/blob/master/examples/configure-help.js
+//              https://github.com/tj/commander.js/blob/master/examples/custom-help-description
+const clone = program.command('clone <source> [destination]');
+clone
+    .option('-f, --force', '是否强制克隆')
+    .description('clone a repository into a newly created directory')
+    .action((source, destination, option) => {
+        console.log(source, destination, option.force);
+    });
+// imooc-test-berners clone https://github.com/tj/commander.js.git 开发脚手架工具 --force
+
+
+// 注册command命令为子命令，方式2, 注册一个子命令,方便扩展
+const service = new Command('service');
+service
+    .command('start [port]')
+    .description('start service at some port')
+    .action((port) => {
+        console.log(port);
+    });
+    //imooc-test-berners service start 3000
+service
+    .command('stop')
+    .description('stop service')
+    .action(() => {
+        console.log('stop service');
+    });
+    // imooc-test-berners service stop
+// 添加这个子命令
+program.addCommand(service);
+
+
+
+// 接收任意命令
+// program
+//     .arguments('<cmd> [options]')
+//     .description('test command', {
+//         cmd: 'command to run',
+//         options: 'options for command',
+//     })
+//     .action((cmd, env) => {
+//         console.log('arguments',cmd, env);
+//     });
+
+
+// 带执行命令
+program
+.command('install [name]', 'install package', {
+    executableFile: 'berners-cli', // 当执行 imooc-test-berners install 这个命令时，对应执行 berners-cli 命令。很强大
+    // isDefault: true, // 在不输入任何命令的适合默认执行的命令
+    // hidden: true, // 让这个命令在控制台不显示
+})
+.alias('i'); // 别名
+
+
+// 高级定制1: 自定义 help 信息
+// console.log(program.helpInformation());
+// program.helpInformation = () => ''; // 先清空输出
+// program.on('--help', () => {
+//     console.log('your help information'); // 自定义输出命令
+// });
+
+
+// 高级定制2: 自定义 help 信息, 这个命令早去其他命令执行
+program.on('option:debug', () => {
+    const options = program.opts();
+    if (options.debug) {
+        process.env.LOG_LEVEL = 'verbose'; // 修改日志级别
+    }
+    // console.log('debug', options.debug);
+    console.log(process.env.LOG_LEVEL);
+});
+
+
+//  高级定制3: 对位置命令监听
+program.on('command:*', (obj) => { // imooc-test-berners aaa
+    console.log(obj); // [ 'aaa' ] 不可用用的命令
+    console.log( program.commands.map(cmd => cmd.name()) ); // 可用的命令
+});
+
+
+// 这句话要写在结尾， 解析参数
+program.parse(process.argv);
